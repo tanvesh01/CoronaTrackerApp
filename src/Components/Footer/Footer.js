af783bcc763220f3a9c086b753567b2b308cecbb
tanvesh <sarvetanvesh01@gmail.com>
2020-08-08 11:42:07 +0530

    typos

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,48 +1,52 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import GitHubIcon from '@material-ui/icons/GitHub';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import GitHubIcon from "@material-ui/icons/GitHub";
 function Copyright() {
     return (
-        <Typography variant="body2" style={{
-            textAlign: "center"
-        }} color="textSecondary">
-            {'Copyright © '}
+        <Typography
+            variant="body2"
+            style={{
+                textAlign: "center",
+            }}
+            color="textSecondary"
+        >
+            {"Copyright © "}
             <Link color="inherit" href="https://material-ui.com/">
                 Corona Tracker App
-      </Link>{' '}
+            </Link>{" "}
             {new Date().getFullYear()}
-            {'.'}
+            {"."}
         </Typography>
     );
 }
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '30vh',
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "30vh",
     },
     footer: {
         padding: theme.spacing(2, 1),
-        marginTop: 'auto',
+        marginTop: "auto",
         backgroundColor:
-            theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+            theme.palette.type === "light" ? theme.palette.grey[200] : theme.palette.grey[800],
     },
     link: {
         fontSize: "1.1rem",
         marginTop: "1px",
-        zIndex: -1
+        zIndex: -1,
     },
     credits: {
         display: "flex",
-        justifyContent: "space-around"
-    }
+        justifyContent: "space-around",
+    },
 }));
 
 export default function StickyFooter() {
@@ -55,33 +59,50 @@ export default function StickyFooter() {
                 <Container maxWidth="sm">
                     <div className={classes.credits}>
                         <div>
-                            <Typography variant="body1">Developed by {" "}
+                            <Typography variant="body1">
+                                Developed by{" "}
                                 <div>
                                     Tanvesh sarve{" "}
                                     <div>
-                                        <Link className={classes.link} color="inherit" href="https://github.com/tanvesh01" >
+                                        <Link
+                                            className={classes.link}
+                                            color="inherit"
+                                            href="https://github.com/tanvesh01"
+                                        >
                                             <GitHubIcon />
-                                        </Link> {" "}
-                                        <Link className={classes.link} color="inherit" href="https://www.instagram.com/stanvesh/" >
+                                        </Link>{" "}
+                                        <Link
+                                            className={classes.link}
+                                            color="inherit"
+                                            href="https://www.instagram.com/stanvesh/"
+                                        >
                                             <InstagramIcon />
-                                        </Link> {" "}
-                                        <Link className={classes.link} color="inherit" href="https://twitter.com/Sarve___tanvesh" >
+                                        </Link>{" "}
+                                        <Link
+                                            className={classes.link}
+                                            color="inherit"
+                                            href="https://twitter.com/Sarve___tanvesh"
+                                        >
                                             <TwitterIcon />
                                         </Link>
                                     </div>
                                 </div>
                             </Typography>
                         </div>
-                        <div >
-                            <Typography variant="body1"> Illustration by {" "}
+                        <div>
+                            <Typography variant="body1">
+                                {" "}
+                                Illustration by{" "}
                                 <div>
-                                    illusna{" "}
-                                    <div >
-
-                                        <Link className={classes.link} color="inherit" href="https://www.instagram.com/illusna/" >
+                                    ashdoesign{" "}
+                                    <div>
+                                        <Link
+                                            className={classes.link}
+                                            color="inherit"
+                                            href="https://www.instagram.com/illusna/"
+                                        >
                                             <InstagramIcon />
                                         </Link>
-
                                     </div>
                                 </div>
                             </Typography>
